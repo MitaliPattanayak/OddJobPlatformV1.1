5302c763b1eaa5c99bd72da9daa7e767e7517cb1
MitaliPattanayak <33121293+MitaliPattanayak@users.noreply.github.com>
2017-12-04 01:38:20 +0000

    New MVC Application created

---
+++ OddJobPlatformV1.1/Controllers/JobsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OddJobPlatformV1._1.Models;
+using System.Threading.Tasks;
+
+namespace OddJobPlatformV1._1.Controllers
+{
+    public class JobsController : Controller
+    {
+        private JobDBContext db = new JobDBContext();
+
+        // GET: Jobs
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Jobs.ToListAsync());
+        }
+
+        // GET: Jobs/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = await db.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // GET: Jobs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "jobId,jobName,jobType,JobDescription,contactEmail,contactNum,salary,address,expiryDate")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jobs.Add(job);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(job);
+        }
+
+        // GET: Jobs/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = await db.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // POST: Jobs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "jobId,jobName,jobType,JobDescription,contactEmail,contactNum,salary,address,expiryDate")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(job).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(job);
+        }
+
+        // GET: Jobs/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Job job = await db.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return HttpNotFound();
+            }
+            return View(job);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Job job = await db.Jobs.FindAsync(id);
+            db.Jobs.Remove(job);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OddJobPlatformV1.1/Models/Job.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OddJobPlatformV1._1.Models
+{
+    public class Job
+    {
+        public int jobId { get; set; }
+        public string jobName { get; set; }
+        public string jobType { get; set; }
+        public string JobDescription { get; set; }
+        public string contactEmail { get; set; }
+        public string contactNum { get; set; }
+        public int salary { get; set; }
+        public string address { get; set; }
+        public DateTime expiryDate { get; set; }
+
+    }
+
+    public class JobDBContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
